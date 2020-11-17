b46b31fa8a8ec1cb9b19981e252e33c07b9f5119
luis Velito <luisvelito@luiss-MacBook-Pro-2.local>
2020-11-16 20:19:56 -0500

    build(fix): stop propagation of event

--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,14 @@
 var triggers = document.getElementsByClassName("trigger-tabs");
+var addTriggers = document.getElementsByClassName("addTrigger");
 var tabs = document.getElementsByClassName("items-tabs");
 
 window.onload = function () {
   for (let i = 0; i < triggers.length; i++) {
     triggers[i].addEventListener("click", changeTab);
+  }
+
+  for (let i = 0; i < addTriggers.length; i++) {
+    addTriggers[i].addEventListener("click", removeElement, true);
   }
 };
 
@@ -22,4 +27,13 @@ function changeTab(event) {
   element.classList.add("color--blue");
   element.classList.add("font-bold");
   tab.classList.add("tabs-active");
+}
+
+function removeElement() {
+  event.stopPropagation();
+  // Removes an element from the document
+  console.log("element to get");
+  let elementId = this.getAttribute("rel");
+  let element = document.getElementById(elementId);
+  element.parentNode.removeChild(element);
 }
